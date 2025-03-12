d67116aea710ae2b2b75dab90c2c0715eed239d7
Darshan MS <shainworks033@gmail.com>
2025-03-12 20:40:59 +0530

    Update mainpromise.js

--- Node.js Notes/fsndpathmodules/mainpromise.js
+++ Node.js Notes/fsndpathmodules/mainpromise.js
@@ -1,4 +1,20 @@
-import fs from "fs/promises"
-let a = await fs.readFile("darshan.txt")
-let b = await fs.appendFile("darshan.txt","\n\n Hey i am written using promises it's so easy without any callbacks")
-console.log(a.toString(),b)
+const fs = require("fs").promises;
+
+async function handleFiles() {
+    console.log("Starting");
+
+    await fs.writeFile("darshan2.txt", "Hey darsshan here with 2.0 version");
+    console.log("File written");
+
+    let data = await fs.readFile("darshan2.txt", "utf-8");
+    console.log("File read:", data);
+
+    await fs.appendFile("darshan.txt", "Hey I am an appended text");
+    console.log("Text appended");
+
+    console.log("Ending");
+}
+
+handleFiles();
+console.log("This runs while async function executes");
+//Here i am using this module "require("fs").promises" to make it use async and await to avoid callbackhell
